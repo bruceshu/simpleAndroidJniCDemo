7c3bb1ecbb5424aacf6bdd60bd0c862b30560b20
bruceshu <3350207067@qq.com>
2018-12-17 10:48:54 +0800

    correct compile right

--- testLib/compile_test.sh
+++ testLib/compile_test.sh
@@ -1,10 +1,4 @@
-/*
-Copyright (c) bruce 3350207067@qq.com
-Auther: bruce
-Date: 2018/12/17
-Description:
-
-*/
+#! /usr/bin/env bash
 
 if [ -z "$ANDROID_NDK" ]; then
     echo "You must define ANDROID_NDK before starting."
